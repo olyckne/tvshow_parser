62c104236939850d1047ee364163cf4fac856c05
Mattias Lyckne <mattias.lyckne@gmail.com>
2013-02-04 21:35:17 +0100

    Added skeleton Trakt class

---
+++ modules/metadata/trakt.py
@@ -0,0 +1,13 @@
+from metadata import *
+
+
+class Trakt(Metadata):
+
+    def __init__(self, config):
+        super(Trakt, self).__init__(config)
+
+    def getEpisodeInfo(self, serie, season, episode):
+        pass
+
+    def getArtwork(serie, season):
+        pass
