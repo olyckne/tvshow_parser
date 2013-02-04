7a4e417331e39c14e999aa9ac26f7dc9b8d02805
Mattias Lyckne <mattias.lyckne@gmail.com>
2013-02-04 23:29:55 +0100

    checkIfVideo (mkv,avi,mp4 or m4v format)

--- modules/file.py
+++ modules/file.py
@@ -1,4 +1,13 @@
+import os
 class File(object):
 
     def __init__(self, config):
         self.config = config
+
+    def checkIfVideo(self):
+        formats = ["mkv", "avi", "mp4", "m4v"]
+        extension = os.path.splitext(self.config['file']['name'])[1][1:]
+
+        return extension in formats
+
+
