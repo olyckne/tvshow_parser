8af3ca185dfb167884d815319b03495b46dbe6d2
Mattias Lyckne <mattias.lyckne@gmail.com>
2013-02-04 23:30:38 +0100

    move file to temp dir

--- modules/file.py
+++ modules/file.py
@@ -1,4 +1,9 @@
 import os
+import tempfile
+import shutil
+import sys
+
+
 class File(object):
 
     def __init__(self, config):
@@ -10,4 +15,30 @@ class File(object):
 
         return extension in formats
 
+
+    def moveToTemp(self):
+        temp = self.config['temp']['path'] + self.config['file']['name'] + "/"
+
+        if os.path.exists(temp):
+            print "OK!"
+        else:
+            self.path = tempfile.mkdtemp()
+
+        filepath = self.config['file']['path'] + "/" + self.config['file']['name']
+        filesize = os.stat(filepath).st_size
+        copied = 0
+        source = open(filepath, "rb")
+        target = open(self.path + "/" + self.config['file']['name'], "wb")
+        while True:
+            chunk = source.read(32768)
+            if not chunk:
+                break
+            target.write(chunk)
+            copied += len(chunk)
+            sys.stdout.write("\r\r%d%%" % (copied * 100 / filesize))
+            sys.stdout.flush()
+
+        source.close()
+        target.close()
+        os.chdir(self.path)
 
