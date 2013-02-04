9ec967392ab6e0f2e8e3309c9e55edd794c1e79a
Mattias Lyckne <mattias.lyckne@gmail.com>
2013-02-04 19:26:15 +0100

    getopt for arguments

--- tvshow-parser.py
+++ tvshow-parser.py
@@ -1,5 +1,26 @@
 import sys
+import getopt
 import yaml
+
+shortArgs = {
+    "h": "help",
+    "c:": "config file",
+    "d:": "debug level",
+}
+
+longArgs = {
+    "type": "type of media - TV/Movie - Default: TV",
+    "temp-dir": "temp directory to use - Default: ./temp",
+    "meta-module": "metadata module to use - Default trakt",
+    "add-module": "add module to use - Default: itunes",
+    "sub-module": "subtitle module to use - Default: subliminal",
+    "sub": "subtitle file to use - Default srt/sub with same filename as input",
+    "notification-module": "notification module to use - Default: prowl",
+    "season": "Set season # - Default: Parse from filename",
+    "episode": "Set episode # - Default: Parse from filename",
+    "name": "Set name - Default: Parse from filename",
+    "year": "Set year - Default: Parse from filename"
+}
 
 
 def loadConfig(file):
@@ -13,8 +34,33 @@ def loadConfig(file):
     return config
 
 
+def usage():
+    print "tvshow-parser"
+
+    for arg in shortArgs:
+        print "{0:25} {1:15s}".format("-" + arg.replace(":", ""), shortArgs[arg])
+
+    for arg in longArgs:
+        print "{0:25} {1:15s}".format("--" + arg, longArgs[arg])
+
+
 def main(argv):
     configFile = "config.yaml"
+    try:
+        opts, args = getopt.getopt(argv, str(shortArgs.keys()), longArgs.keys())
+    except getopt.GetoptError:
+        usage()
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            usage()
+            sys.exit()
+        elif opt == "-c":
+            configFile = arg
+
+    filename = "".join(args)
+
     config = loadConfig(configFile)
 
     if not config:
