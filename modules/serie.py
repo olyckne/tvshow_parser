f541411190f7bf92ff0609e7dae6bf14feede664
Mattias Lyckne <mattias.lyckne@gmail.com>
2013-02-12 20:12:53 +0100

    regex for TV.show.name.01x01. pattern

--- modules/serie.py
+++ modules/serie.py
@@ -8,14 +8,27 @@ class Serie(object):
     def parseFilename(self, filename=False):
         if not filename:
             self.filename = self.config['file']['name']
-        pattern = "'^(.+)\.(S?([0-9]|[0-9]){1,2})(E?([0-9]|[0-9]){1,2})(\.|-).*$'i"
+#        pattern = "'^(.+)\.(S?([0-9]|[0-9]){1,2})(E?([0-9]|[0-9]){1,2})(\.|-).*$'i"
+#
+#        #
+#        # TV.Show.Name.SxxExx.mkv
         pattern = r"^(?P<name>(.*))\.(?:S)(?P<season>([0-9])*)(?:E)(?P<episode>([0-9])*)"
         matches = re.search(pattern, self.filename)
+        if not matches:
+            ## TV.Show.Name.01x01.mkv
+            pattern = r"^(?P<name>(.*))\.(?P<season>([0-9])*)(?:x)(?P<episode>([0-9])*)"
+
+            matches = re.search(pattern, self.filename)
+
         if matches:
             self.name = matches.group("name")
             self.name = re.sub(r"[._-]", " ", self.name)
             self.season = matches.group("season")
             self.episode = matches.group("episode")
+        else:
+            self.name = self.filename
+            self.season = 0
+            self.episode = 0
 
         if re.search(r"(720|1080)p", self.filename):
             self.hd = True
