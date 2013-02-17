e069daa2343e52cecd870a104f9d135589ecfd7b
Mattias Lyckne <mattias.lyckne@gmail.com>
2013-02-17 19:37:36 +0100

    __init__ in notification module

--- modules/notification/notification.py
+++ modules/notification/notification.py
@@ -5,6 +5,9 @@ class Notification(object):
 
     __metaclass__ = abc.ABCMeta
 
+    def __init__(self, config):
+        self.config = config
+
     @abc.abstractmethod
     def sendNotification(self):
         pass
