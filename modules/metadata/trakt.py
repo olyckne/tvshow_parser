c358124d0aed1325c5d7609e4f8e0fd4bdd4e0e1
Mattias Lyckne <mattias.lyckne@gmail.com>
2013-02-17 23:02:38 +0100

    Working getEpisodeInfo for Trakt.

--- modules/metadata/trakt.py
+++ modules/metadata/trakt.py
@@ -1,4 +1,7 @@
 from metadata import *
+import httplib2
+import json
+import datetime
 
 
 class Trakt(Metadata):
@@ -17,8 +20,47 @@ class Trakt(Metadata):
         self.trakt = config['trakt']
         super(Trakt, self).__init__(config)
 
-    def getEpisodeInfo(self, serie, season, episode):
-        pass
+    def getEpisodeInfo(self, name, season, episode):
+        serie = {
+            "name": name,
+            "season": season,
+            "episode": episode
+        }
+
+        url = self.constructUrl("episode", [name, season, episode])
+        cacheDir = self.config['temp']['path'] + "/.cache" if "temp" in self.config else ".cache"
+        h = httplib2.Http(cacheDir)
+        resp, content = h.request(url, "GET")
+        content = json.loads(content)
+        if not "status" in content:
+            show = content['show']
+            episode = content['episode']
+            serie['poster'] = show['images']['poster']
+            serie['desc'] = episode['overview'].replace('"', "'")
+            serie['epName'] = episode['title'].replace('"', "'")
+            serie['year'] = datetime.date.fromtimestamp(episode['first_aired']).isoformat()
+            genres = show['genres']
+            serie['genre'] = genres[0]
+            genres.pop(0)
+
+            serie['comments'] = ", ".join(genres)
+            serie['id'] = episode['tvdb_id']
+
+            url = self.constructUrl("season", [name])
+            resp, content = h.request(url, "GET")
+            content = json.loads(content)
+
+            if not "status" in content:
+                for s in content:
+                    if int(serie['season']) == int(s['season']):
+                        serie['nrOfEpisodes'] = s['episodes']
+                        break
+        else:
+            print "no data"
+        print url
+
+        print serie
+        return serie
 
     def getArtwork(serie, season):
         pass
