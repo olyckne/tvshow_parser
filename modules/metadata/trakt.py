601258e91b2b89a8aaf8e5ed190efdcbbaf4ace7
Mattias Lyckne <mattias.lyckne@gmail.com>
2013-02-17 22:58:29 +0100

    Added URLs to Trakt

--- modules/metadata/trakt.py
+++ modules/metadata/trakt.py
@@ -3,6 +3,13 @@ from metadata import *
 
 class Trakt(Metadata):
 
+    baseUrl = "http://api.trakt.tv/"
+    url = {
+        "episode": baseUrl + "show/episode/summary.json/",
+        "season": baseUrl + "show/seasons.json/",
+        "watchlist": baseUrl + "show/episode/watchlist/"
+    }
+
     def __init__(self, config):
         super(Trakt, self).__init__(config)
 
