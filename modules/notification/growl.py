676cd7bed78ba5234287576487a1d05679fb50d1
Mattias Lyckne <mattias.lyckne@gmail.com>
2013-02-17 19:45:29 +0100

    syntax fixes

--- modules/notification/growl.py
+++ modules/notification/growl.py
@@ -8,9 +8,9 @@ class Growl(Notification, gntp.notifier.GrowlNotifier):
     def __init__(self, config):
         Notification.__init__(self, config)
         gntp.notifier.GrowlNotifier.__init__(self,
-             applicationName = tvshow_parser.__app_name__,
-             notifications = ["Parsing done"],
-             defaultNotifications = ["Parsing done"],
+            applicationName=tvshow_parser.__app_name__,
+            notifications=["Parsing done"],
+            defaultNotifications=["Parsing done"],
             )
         self.register()
 
