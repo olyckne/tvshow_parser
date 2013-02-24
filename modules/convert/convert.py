f97076b2e01121f86280175b3b9d34d2c015a228
Mattias Lyckne <mattias.lyckne@gmail.com>
2013-02-25 00:55:32 +0100

    Convert __init__ takes config

--- modules/convert/convert.py
+++ modules/convert/convert.py
@@ -4,6 +4,9 @@ import abc
 class Convert(object):
     __metaclass__ = abc.ABCMeta
 
+    def __init__(self, config):
+        self.config = config
+
     @abc.abstractmethod
     def convert(self):
         pass
