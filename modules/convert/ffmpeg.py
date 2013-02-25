cab91e565a6424da60a01dfefe315e396a864605
Mattias Lyckne <mattias.lyckne@gmail.com>
2013-02-25 20:11:49 +0100

    ffmpeg __init__ that searches for ffmpeg command

--- modules/convert/ffmpeg.py
+++ modules/convert/ffmpeg.py
@@ -1,7 +1,21 @@
 from convert import *
+import os
 
 
 class Ffmpeg(Convert):
+
+    def __init__(self, config):
+        self.__ffmpeg__ = False
+        for path in os.getenv("PATH").split(os.pathsep):
+            path = os.path.join(path, "ffmpeg")
+            if os.access(path, os.X_OK):
+                self.__ffmpeg__ = path
+
+        if not self.__ffmpeg__:
+            raise Exception("ffmpeg not found?! Exiting")
+            sys.exit(1)
+
+        super(Ffmpeg, self).__init__(config)
 
     def convert(self):
         pass
