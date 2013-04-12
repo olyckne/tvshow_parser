9b07a7724daec5d0f5e3b303e319c2024788b2cb
Mattias Lyckne <mattias.lyckne@gmail.com>
2013-04-13 00:44:12 +0200

    added regex movie

--- modules/movie.py
+++ modules/movie.py
@@ -6,4 +6,14 @@ class Movie(object):
         self.config = config
 
     def parseFilename(self, filename=False):
-        print filename
+        if not filename:
+            if "metadata" in self.config['file'] and \
+            "filename" in self.config['file']['metadata']:
+                self.filename = self.config['file']['metadata']['filename']
+            else:
+                self.filename = self.config['file']['name']
+        pattern = r"^(?P<name>(.*))\.(?P<year>([0-9])*)"
+        matches = re.search(pattern, self.filename)
+
+        print matches.group("name")
+        print matches.group("year")
